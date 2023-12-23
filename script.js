31e294797b8f82963a283c066ee97311fd3073b6
Ryan <griffinryan77@gmail.com>
2023-12-22 20:42:49 -0500

    button joke added

---
+++ script.js
@@ -0,0 +1,11 @@
+function addingEventListener() {
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('Imagine falling for that HEHEHE!');
+}
+
+input.addEventListener('click', clickAlert);
+}
+
+console.log(addingEventListener());
